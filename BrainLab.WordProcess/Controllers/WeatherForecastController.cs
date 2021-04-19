eaefa3266fee03198435e053454f8c0c293e874f
Elad Ezra <elad-ez@yedioth.co.il>
2021-04-19 11:58:55 +0300

    Added welcome message

--- BrainLab.WordProcess/Controllers/WeatherForecastController.cs
+++ BrainLab.WordProcess/Controllers/WeatherForecastController.cs
@@ -11,11 +11,6 @@ namespace BrainLab.Feeds_processing.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +19,9 @@ namespace BrainLab.Feeds_processing.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Welcome to the feed-processing API. Please send your requests";
         }
     }
 }
